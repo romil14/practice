18c55f28d2793eba4c6e51b7a159e87c2a44abb4
Romil Chandragade <Romil.Raveendra.Chandragad@gds.ey.com>
2022-02-14 17:39:12 +0530

    Added Middleware Project

---
+++ Practice/Practice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            for (int i = 0; i < 20; i++)
+            {
+                DemoClass obj = new DemoClass();
+                obj.Id = i + 10;
+                obj.Name = "Object " + i;
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class DemoClass
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
