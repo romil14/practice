fe642856b96cddd1af5a43e5086a4e73e9110b10
Romil Chandragade <Romil.Raveendra.Chandragad@gds.ey.com>
2022-02-15 10:55:55 +0530

    Added custome middleware in separate file

---
+++ Practice/Middleware/Custom/Middleware/CustomMiddleware1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Middleware.Custom.Middleware
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class CustomMiddleware1
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomMiddleware1(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            //To Do Code..
+
+            //httpContext.Response.WriteAsync("Custome Middleware!");
+            //return _next(httpContext);
+
+            await httpContext.Response.WriteAsync("Custome Middleware!");
+            await _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class CustomMiddleware1Extensions
+    {
+        public static IApplicationBuilder UseCustomMiddleware1(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CustomMiddleware1>();
+        }
+    }
+}
--- Practice/Middleware/Startup.cs
+++ Practice/Middleware/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Middleware.Custom.Middleware;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,6 +31,7 @@ namespace Middleware
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCustomMiddleware1();
 
             ////Custom Middleware 
             //app.Use(async (context, next) =>
