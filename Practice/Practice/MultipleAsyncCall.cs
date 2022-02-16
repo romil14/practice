722e0b77f725a65b3dcaf4d9347fa85f064dcbb4
Romil Chandragade <Romil.Raveendra.Chandragad@gds.ey.com>
2022-02-16 17:25:54 +0530

    Added code for Multiple Async Call

---
+++ Practice/Practice/MultipleAsyncCall.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    public class MultipleAsyncCall
+    {
+        public async Task<List<string>> GetFbName()
+        {
+            await Task.Delay(5000);
+            return new List<string>() { "FB1", "FB2", "FB3" };
+        }
+
+        public async Task<List<string>> GetInstaName()
+        {
+            await Task.Delay(3000);
+            return new List<string>() { "Insta1", "Insta2", "Insta3" };
+        }
+
+        public async Task<List<string>> GetData()
+        {
+            var result = new List<string>();
+            var fbData = await GetFbName();
+            var instaData = await GetInstaName();
+
+            result.AddRange(fbData);
+            result.AddRange(instaData);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            //Task<List<string>> fbTaskData = GetFbName();
+            //Task<List<string>> instaTaskData = GetInstaName();
+
+            //await Task.WhenAll(fbTaskData, instaTaskData);
+
+           
+            //result.AddRange(fbTaskData.Result);
+            //result.AddRange(instaTaskData.Result);
+
+
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            return result;
+
+        }
+    }
+}
--- Practice/Practice/Program.cs
+++ Practice/Practice/Program.cs
@@ -1,18 +1,34 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace Practice
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            int a = 10;
-            for (int i = 0; i < 20; i++)
-            {
-                DemoClass obj = new DemoClass();
-                obj.Id = i + 10;
-                obj.Name = "Object " + i;
-            }
+            //int a = 10;
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    DemoClass obj = new DemoClass();
+            //    obj.Id = i + 10;
+            //    obj.Name = "Object " + i;
+            //}
+
+            //Demo3 d3 = new Demo3();
+            //IDemo2 id2 = new Demo3();
+            //IDemo3 id3 = new Demo3();
+
+            //id2.GetName();
+            //id3.GetName();
+
+            MultipleAsyncCall multipleAsyncCall = new MultipleAsyncCall();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var data = await multipleAsyncCall.GetData();
+            stopwatch.Stop();
+            Console.WriteLine($"Time taken: {stopwatch.Elapsed.ToString(@"m\:ss\.fff")}");
             Console.WriteLine("Hello World!");
         }
     }
@@ -22,5 +38,65 @@ namespace Practice
         public int Id { get; set; }
 
         public string Name { get; set; }
+    }
+
+    public interface IDemo1
+    {
+
+        int GetId();
+    }
+
+    public class Demo1
+    {
+        public virtual void GetName()
+        {
+            Console.WriteLine("From Demo1 Class"); 
+        } 
+    }
+
+    public class Demo2 : Demo1
+    {
+        public override void GetName()
+        {
+            Console.WriteLine("From Demo2 Class");
+        }
+    }
+
+    public interface IDemo2
+    {
+        void GetName();
+    }
+
+    public interface IDemo3
+    {
+      
+        void GetName();
+    }
+
+    public class Demo3 : IDemo2, IDemo3
+    {
+         void IDemo2.GetName()
+        {
+            Console.WriteLine("From IDemo2.GetName()");
+        }
+
+          void IDemo3.GetName()
+        {
+            Console.WriteLine("From IDemo3.GetName()");
+        }
+
+        public void TestName()
+        {
+           
+            Console.WriteLine("From TestName Method");
+        }
+    }
+
+    public class Demo4
+    {
+        public void GetName()
+        {
+            Console.WriteLine("From Demo4 Class");
+        }
     }
 }
